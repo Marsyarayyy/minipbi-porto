3ca9651bc72ac679f205cba74e495d6a230fe5d2
marsyarayyy <marsyarh09@gmail.com>
2025-02-20 17:40:26 +0700

    benerin bug1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div>
       <div className='flex flex-row px-10 py-5 items-center justify-between bg-[#040812] text-white font-bold sticky top-0 z-50' >
-         <Image src="/logo.png" alt="logo" width={800} height={800}  />
+         <Image src="/logo.png" alt="logo" width={800} height={800}  /> 
     
       <div className='flex flex-row gap-10 items-center justify-center text-[18px] font-medium'>
           <a href="/  ">About</a>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Shrikhand } from "next/font/google";
-import "./globals.css"; // Pastikan Tailwind diimport
-
-const shrikhand = Shrikhand({ subsets: ["latin"], weight: "400" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
